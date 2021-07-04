8742a6875d7ac395a011dadb7158e8c16b9b3c9e
tncamp <80801111+tncamp@users.noreply.github.com>
2021-07-04 02:07:02 -0500

    Create auth.js
    Required jsonwebtoken
    Required passport

---
+++ auth.js
@@ -0,0 +1,36 @@
+const jwtSecret = 'your_jwt_secret';       //has to be same key used in JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport');       //local file
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username,      //username encoding in the JWT
+    expiresIn: '7d',
+    algorithm: 'HS256'    //algorithm used to sign or encode values of JWT
+  });
+}
+
+/* POST login */
+
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+              return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+              });
+            }
+            req.login(user, { session: false }, (error) => {
+              if (error) {
+                res.send(error);
+              }
+              let token = generateJWTToken(user.toJSON());
+              return res.json({ user, token });
+            });
+          })(req, res);
+        });
+      }

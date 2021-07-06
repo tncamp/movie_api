f311d7dab65da2628926af7cadeb85f49f253e6b
tncamp <80801111+tncamp@users.noreply.github.com>
2021-07-06 18:54:10 -0500

    Update models.js
    Required bcrypt
    Password validation

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
@@ -23,6 +24,14 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movies' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let directorSchema = mongoose.Schema({
   Name: {type: String, required: true},

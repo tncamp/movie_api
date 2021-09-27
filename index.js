db89383ffcb5cce96ee1970cfe2d4431b174a9f1
tncamp <80801111+tncamp@users.noreply.github.com>
2021-09-26 22:09:21 -0500

    Removed passport authenticate

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 //get list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   movies.find()
     .then((movies) => {
       res.status(201).json(movies);
